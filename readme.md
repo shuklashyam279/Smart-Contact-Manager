06924febaa89a770b63f367daf095248f2bafead
Shyam Shukla <shuklashyam279@gmail.com>
2024-11-22 13:52:49 +0530

    configure Tailwind

---
+++ readme.md
@@ -0,0 +1,3 @@
+# Run the cmd everytime when making changes to the css file
+
+npx tailwindcss -i ./src/main/resources/static/css/input.css -o ./src/main/resources/static/css/output.css --watch
--- src/main/java/com/scm/demo/controller/PageController.java
+++ src/main/java/com/scm/demo/controller/PageController.java
@@ -1,5 +1,19 @@
 package com.scm.demo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class PageController {
 
+@RequestMapping("/home")
+    public String home(Model model) {
+        System.out.println("Home page handler");
+        // sending data to view
+        model.addAttribute("name", "Substring Technologies");
+        model.addAttribute("youtubeChannel", "Learn Code With Durgesh");
+        model.addAttribute("githubRepo", "https://github.com/learncodewithdurgesh/");
+        return "home";
+    }
 }
