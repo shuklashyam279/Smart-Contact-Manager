230de31d04ffbeb47770ff44adc490cfeed72acc
Shyam Shukla <shuklashyam279@gmail.com>
2024-11-22 17:26:18 +0530

    dark and light done

--- src/main/java/com/scm/demo/controller/PageController.java
+++ src/main/java/com/scm/demo/controller/PageController.java
@@ -2,6 +2,7 @@ package com.scm.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -17,17 +18,37 @@ public class PageController {
         return "home";
     }
 
-	// about route
+    // about route
+
     @RequestMapping("/about")
     public String aboutPage(Model model) {
         model.addAttribute("isLogin", true);
         System.out.println("About page loading");
         return "about";
     }
+
     // services
+
     @RequestMapping("/services")
     public String servicesPage() {
         System.out.println("services page loading");
         return "services";
+    }
+
+    // contact page
+
+    @GetMapping("/contact")
+    public String contact() {
+        return new String("contact");
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return new String("login");
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "register";
     }
 }
