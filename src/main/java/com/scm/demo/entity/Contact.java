1ca59dcace67567d867d7ea7874ab9901fe00c64
Shyam Shukla <shuklashyam279@gmail.com>
2024-11-24 01:12:37 +0530

    [Feature] Add contact form

--- src/main/java/com/scm/demo/entity/Contact.java
+++ src/main/java/com/scm/demo/entity/Contact.java
@@ -10,8 +10,16 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Contact {
 
     @Id
@@ -21,8 +29,10 @@ public class Contact {
     private String phoneNumber;
     private String address;
     private String picture;
+
     @Column(length = 1000)
     private String description;
+    
     private boolean favorite = false;
     private String websiteLink;
     private String linkedInLink;
@@ -32,5 +42,4 @@ public class Contact {
 
     @OneToMany(mappedBy = "contact", cascade = CascadeType.ALL, fetch = FetchType.EAGER, orphanRemoval = true)
     private List<SocialLink> links = new ArrayList<>();
-
 }
